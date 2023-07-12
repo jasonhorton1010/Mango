c53e1fb319e127e4efd45a6b40ad5ee5da5b0ed1
Jason Horton <jasonhorton1010@googlemail.com>
2023-07-12 16:38:58 +0100

    Add project files.

---
+++ Mango.Services.CouponAPI/Models/Dtos/ResponseDTO.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion.Internal;
+using System.Diagnostics.Eventing.Reader;
+
+namespace Mango.Services.CouponAPI.Models.Dtos
+{
+    public class ResponseDTO
+    {
+        public object? Result { get; set; }
+
+        public bool IsSuccess { get; set; }
+
+        public string Message { get; set; }
+    }
+}
