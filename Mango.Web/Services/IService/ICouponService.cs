9f8f747f1bdc0d434c1673806fb7af47ab90a4d8
Jason Horton <jasonhorton1010@googlemail.com>
2023-07-13 17:33:02 +0100

    Coupon Service Interface

---
+++ Mango.Web/Services/IService/ICouponService.cs
@@ -0,0 +1,13 @@
+﻿using Mango.Web.Models;
+
+namespace Mango.Web.Services.IService;
+
+public interface ICouponService
+{
+    Task<ResponseDTO> GetCouponAsync(string couponCode);
+    Task<ResponseDTO> GetAllCouponsAsync();
+    Task<ResponseDTO> GetCouponByIdAsync(int id);
+    Task<ResponseDTO> CreateCouponsAsync(CouponDTO couponDTO);
+    Task<ResponseDTO> UpdateCouponsAsync(CouponDTO couponDTO);
+    Task<ResponseDTO> DeleteCouponsAsync(int id);
+}
