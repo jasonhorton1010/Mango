e89a6b8954200492b4848d0e577f58b78e53cf15
Jason Horton <jasonhorton1010@googlemail.com>
2023-07-14 21:02:03 +0100

    IAuth and Auth Services

---
+++ Mango.Services.AuthAPI/Services/AuthService.cs
@@ -0,0 +1,32 @@
+﻿using Mango.Services.AuthAPI.Data;
+using Mango.Services.AuthAPI.Models;
+using Mango.Services.AuthAPI.Models.DTOs;
+using Mango.Services.AuthAPI.Services.IService;
+using Microsoft.AspNetCore.Identity;
+
+namespace Mango.Services.AuthAPI.Services
+{
+    public class AuthService : IAuthService
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public AuthService(ApplicationDbContext db, UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            _db = db;
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        public Task<LoginRequestDTO> Login(LoginRequestDTO requestDTO)
+        {
+            
+        }
+
+        public Task<UserDTO> Register(RegistrationRequestDTO reqistrationRequestDTO)
+        {
+            
+        }
+    }
+}
---
+++ Mango.Services.AuthAPI/Services/IService/IAuthService.cs
@@ -0,0 +1,11 @@
+﻿using Mango.Services.AuthAPI.Models.DTOs;
+
+namespace Mango.Services.AuthAPI.Services.IService
+{
+    public interface IAuthService
+    {
+        Task<UserDTO> Register(RegistrationRequestDTO reqistrationRequestDTO);
+
+        Task<LoginRequestDTO> Login(LoginRequestDTO requestDTO);
+    }
+}
