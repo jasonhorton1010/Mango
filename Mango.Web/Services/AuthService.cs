c92a013aaafd1893f870667cd0447ab460e5da5f
Jason Horton <jasonhorton1010@googlemail.com>
2023-07-17 09:23:00 +0100

    Auth Service in Web Project

---
+++ Mango.Web/Services/AuthService.cs
@@ -0,0 +1,47 @@
+﻿using Mango.Web.Models;
+using Mango.Web.Models.DTOs;
+using Mango.Web.Services.IService;
+using Mango.Web.Utility;
+
+namespace Mango.Web.Services
+{
+    public class AuthService : IAuthService
+    {
+        private readonly IBaseService _baseService;
+
+        public AuthService(IBaseService baseService)
+        {
+            _baseService = baseService;
+        }
+
+        public async Task<ResponseDTO?> AssignRoleAsync(RegistrationRequestDTO registrationRequestDTO)
+        {
+            return await _baseService.SendAsync(new RequestDTO()
+            {
+                ApiType = StaticDetails.ApiType.POST,
+                Data = registrationRequestDTO,
+                Url = StaticDetails.AuthAPIBase + "/api/auth/AssignRole"
+            });
+        }
+
+        public async Task<ResponseDTO?> LogInAsync(LoginRequestDTO loginRequestDTO)
+        {
+            return await _baseService.SendAsync(new RequestDTO()
+            {
+                ApiType = StaticDetails.ApiType.POST,
+                Data = loginRequestDTO,
+                Url = StaticDetails.AuthAPIBase + "/api/auth/login"
+            });
+        }
+
+        public async Task<ResponseDTO?> RegisterInAsync(RegistrationRequestDTO registrationRequestDTO)
+        {
+            return await _baseService.SendAsync(new RequestDTO()
+            {
+                ApiType = StaticDetails.ApiType.POST,
+                Data = registrationRequestDTO,
+                Url = StaticDetails.AuthAPIBase + "/api/auth/register"
+            });
+        }
+    }
+}
---
+++ Mango.Web/Services/IService/IAuthService.cs
@@ -0,0 +1,12 @@
+﻿using Mango.Web.Models;
+using Mango.Web.Models.DTOs;
+
+namespace Mango.Web.Services.IService
+{
+    public interface IAuthService
+    {
+        Task<ResponseDTO?> LogInAsync(LoginRequestDTO loginRequestDTO);
+        Task<ResponseDTO?> RegisterInAsync(RegistrationRequestDTO registrationRequestDTO);
+        Task<ResponseDTO?> AssignRoleAsync(RegistrationRequestDTO registrationRequestDTO);
+    }
+}
