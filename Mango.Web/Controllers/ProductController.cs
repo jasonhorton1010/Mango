4695c83301043f39f1ae6af2aa693880af6bc7cb
Jason Horton <jasonhorton1010@googlemail.com>
2023-07-23 14:15:16 +0100

    Assignment - Consuming Product API Part 2 (the UD of CRUD)

--- Mango.Web/Controllers/ProductController.cs
+++ Mango.Web/Controllers/ProductController.cs
@@ -89,5 +89,36 @@ namespace Mango.Web.Controllers
 
             return View(productDto);
         }
+
+        public async Task<IActionResult> ProductEdit(int productId)
+        {
+            ResponseDTO? response = await _productService.GetProductByIdAsync(productId);
+
+            if (response != null && response.IsSuccess)
+            {
+                ProductDto? model = JsonConvert.DeserializeObject<ProductDto>(Convert.ToString(response.Result));
+                return View(model);
+            }
+
+            return NotFound();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ProductEdit(ProductDto productDto)
+        {
+            ResponseDTO? response = await _productService.UpdateProductsAsync(productDto);
+
+            if (response != null && response.IsSuccess)
+            {
+                TempData["success"] = "Product Updated Successfully";
+                return RedirectToAction(nameof(ProductIndex));
+            }
+            else
+            {
+                TempData["error"] = response?.Message;
+            }
+
+            return View(productDto);
+        }
     }
 }
